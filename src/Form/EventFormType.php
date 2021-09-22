4f6c74411d71ede2ae32b22155f2efba35d41ffa
Simon Nedjari <simon.nedjari@outlook.com>
2021-09-22 12:05:32 +0200

    event multiple button 2

--- src/Form/EventFormType.php
+++ src/Form/EventFormType.php
@@ -8,18 +8,25 @@ use App\Entity\Event;
 use App\Entity\Place;
 use App\Entity\Status;
 use App\Entity\User;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class EventFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+
+        $city = new City();
         $builder
             ->add('name')
 
@@ -38,14 +45,32 @@ class EventFormType extends AbstractType
             ->add('duration')
             ->add('infos')
 
-            ->add('place', EntityType::class, [
-                'class' => Place::class,
+
+            ->add("city", EntityType::class, [
+                'mapped' => false,
+                'class' => City::class,
                 'choice_label' => 'name',
+                'placeholder' => 'Cities',
+            ])
+
+            ->add('place',EntityType::class,[
+                'class'=>Place::class,
+                'choice_label'=>'name',
+
+                //Je peux rajouter une requete pour trier la façon dont je vais afficher.
+
+                'query_builder'=>function(EntityRepository $repo) {
+
+                    return $repo->createQueryBuilder('p')
+                        ->andWhere('p.city = 1')
+                        ->addOrderBy('p.name','ASC');
+                }
             ])
 
             ->add('createEvent', SubmitType::class)
             ->add('publishEvent', SubmitType::class)
         ;
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
