7cfdc73c9d466985926947b491e6bc3fac684e7e
d1g1talpump <levirootsmanuva2@gmail.com>
2021-09-22 16:52:08 +0200

    Revert "Merge branch 'master' into branche-alexander"
    This reverts commit c9505b1aea8849a04ef85b605dcffc33424aae16, reversing
    changes made to 6169b58a4324115f8e6c9d3df4e0d772a273a299.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,8 +3,6 @@
 namespace App\Controller;
 
 use App\Repository\EventRepository;
-use http\Client\Curl\User;
-use Monolog\Handler\Handler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,7 +12,6 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="main_home")
      */
-
     public function home(
         EventRepository $eventRepository
     ): Response
@@ -22,17 +19,9 @@ class MainController extends AbstractController
         $allEvents = $eventRepository->allEventsHomePage();
         dump($allEvents);
 
-          
-        return $this->render('main/home.html.twig', ["allEvents" => $allEvents]);
-    }
-
-    /**
-    public function subscribe(EventRepository $eventRepository, HandleRequest $user, Event $event){
-        $event = $this->addUSer($user);
-
-        return $this->render('main/home.html.twig',[
-            "event" => $event
+        return $this->render('main/home.html.twig', [
+            "allEvents" => $allEvents
         ]);
     }
-     **/
+
 }
